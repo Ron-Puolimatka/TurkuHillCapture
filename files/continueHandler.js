7c43faf3ed188bd83bce3c141850c2c268f7dd93
MikeOxlong <Leevis06@outlook.com>
2022-02-12 14:54:28 +0200

    Nothing much

--- files/continueHandler.js
+++ files/continueHandler.js
@@ -1,20 +1,48 @@
 //ei valmis
+StyleSheet
 let input;
 let InputJSON;
 
 function setup() {
+
+  resizeCanvas(windowWidth, windowHeight);
+  noCursor();
   input = createFileInput(handleFile);
-  input.position(0, 0);
+  input.position(80, 140);
+
 }
 
 function draw() {
-  background(255);
+
+  background(0);
+  drawNewCursor();
+
 }
 
 function handleFile(file) {
-    if(file.type === 'JSON')
-    {
-        InputJSON = file;
-        window.open("game.html", "_self");
-    }
+  if(file.type === 'text') {
+    window.open("game.html", "_self"); 
+  }
+}
+
+function drawNewCursor() {
+  
+  stroke(255);
+  
+  if (mouseIsPressed) {
+    strokeWeight(10);
+  }
+  else {
+    strokeWeight(5);
+  }
+    
+  point(mouseX, mouseY);
+  line(mouseX, mouseY, pmouseX, pmouseY);
+  
+}
+
+function loadMainMenu() {
+
+  window.open("menu.html", "_self")
+
 }
